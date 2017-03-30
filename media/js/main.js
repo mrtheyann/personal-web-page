3c4d737a6bb83fad6592f4da0677305cac28dd0f
Yann Pavlenko <mrtheyann@gmail.com>
2017-03-30 08:46:34 +0500

    sync between machines

--- media/js/main.js
+++ media/js/main.js
@@ -1,8 +1,12 @@
-jQuery(document).ready(function() {
-
+$(document).ready(function(){
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('particles-js', './media/assets/particles/config.json', function() {
   console.log('callback - particles.js config loaded');
-  });
+    });
+});
 
+
+$(window).on('load', function() {
+  $('.spinner-wrapper').addClass('hide');
+  $('.banner-wrapper').addClass('loaded');
 });
