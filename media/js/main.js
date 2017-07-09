2e1b1375bcf5aab0f59ee4bcd4728928a9c1864a
mrtheyann <darklodr@gmail.com>
2017-07-09 22:35:58 +0500

    upd: IE/edge ignore

--- media/js/main.js
+++ media/js/main.js
@@ -1,3 +1,5 @@
+var IE = navigator.userAgent.indexOf("Edge") > -1 || navigator.userAgent.indexOf("Trident/7.0") > -1
+
 $(document).ready(function(){
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
   particlesJS.load('particles-js', './media/assets/particles/config.json', function() {
@@ -7,6 +9,7 @@ $(document).ready(function(){
 
 
 $(window).on('load', function() {
+  if (IE) {$('.banner-logo').addClass('hide');}
   $('.spinner-wrapper').addClass('hide');
   $('.banner-wrapper').addClass('loaded');
 });
